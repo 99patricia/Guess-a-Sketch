23a2e08cb4fd0178a54e6b358e29e79d72c47d21
Beikewzh <wzh412000@gmail.com>
2023-03-01 14:33:32 -0700

    aa

--- server/server.js
+++ server/server.js
@@ -14,6 +14,25 @@ const __dirname = path.dirname(__filename);
 const rooms = io.of("/").adapter.rooms;
 const sids = io.of("/").adapter.sids;
 
+// Firebase init
+import { initializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfdLDNlu0qvGBkPNfyow-YcbZqhavQbK4",
+  authDomain: "ece493-capstone.firebaseapp.com",
+  projectId: "ece493-capstone",
+  storageBucket: "ece493-capstone.appspot.com",
+  messagingSenderId: "388992282247",
+  appId: "1:388992282247:web:637b6e6d917685772b5222",
+  measurementId: "G-ESJEZ56XLP"
+};
+
+const FirebaseApp = initializeApp(firebaseConfig);
+
+
+
 app.get('/', (req, res) => {
   res.send('Home Page. Open /chat/chatId')
 });
