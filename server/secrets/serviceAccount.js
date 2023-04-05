fa2a3e18a8399ea3d33893033faea433c37dd4a9
Kyle Bricker <57820561+kbricker8@users.noreply.github.com>
2023-04-05 13:12:22 -0600

    secrets

---
+++ server/secrets/serviceAccount.js
@@ -0,0 +1,15 @@
+export default {
+    type: "service_account",
+    project_id: "ece493-capstone",
+    private_key_id: "3ab7bad39d0f6f07a9f1cc6c20ed2efa54dffad5",
+    private_key:
+        "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC38ez+FonLYA1F\nX3jC2kRlJVMzcGf16qLFbrFiV1YwOo0mGIQwqLfsSnGoSoWz8UYjw4Pnsso1+kWs\nz7bghcMcEQCBaomC6TaNGqgz6MTxpcaipt5XPNIUkjVKDGscaUDjZeVEBGUx1FSW\nA9DmzKxSVNuNMb279FocSgh6K8o6O3Rg3JVtcS+roN5XGsGVvXGI5aLRTFvLNzwL\nqDur0V3VObp1VlfnPcz00unbxL4RkLgFQ44qrWCMqEtyTmNBP62USImBa6CjQRPl\nI5k9xT/bIXfdaueIi9SGnCcVI2PkhZ8R8CYdTT9wGszp039NS9O7kIec6vea2SWc\nwq6V+N/ZAgMBAAECggEADbaHrjaUbbi6rIwORup6NFcowXo1CeCE43aZ20S7DGvl\nB2zG+BCv63nuSLdLietoCPUDsaOCzyzaAmTW05ytO7upv7wnRb7onfLZjMsi2Yf8\nC7aodzT1zWpL9joARzUyjPFU+vHf8GvNpYRhz7787sNcPcWySR+Rky/Su3blw9r3\n9WGw36ZLV1I+a0wsJp4Vkc4d9O9NnBgfU4WiUHKa3DmlFSF7OVM1a7RE4v2oBHfe\n6N3m8Q2Z2npR9doFMDImOueqQCfmpQDIMwXv+hMp3Zw6UxPwysA8946cP2dO5qg4\nVOB8hYyBITbvuOMSE8JZgObzZiNeJL/XlT2aEr4+bQKBgQDgsPwL2gAlR5TC15xq\njFtpOpPY8PT/YRCMBky5QKckgVsdNfJJUeaa7JZ8qzvP9Z63u67GjqQEql+XAi4V\ntcMQspIAniBdlOCvJLeMzDX42JjuQ+zXD9UyQI2xbW7rg8S/s4hVUD9evvFKVTe6\n4Uhd9tmQWkR29zqkHwz9CX2e7QKBgQDRk3hOS/nZsdBB0w/3w2GGXdG9+rxhb+O6\nX/sKolfwAavMiOn6mpwtOrKhSPnYhy6k7vTzrY+rz9TAvgq9RU8ix7QJpoSvzHOW\nKM2qK/60DCFYL595c9obx9PPojNIKUIjIGBu1E9TXXeV2INaHeScR8NAFoE7t6zK\n+VF+Vqp7HQKBgG9ttgFWol3ait65uJtqcebUov+iN43yoBchBKmWc416DSqGV1qf\nIELSc6rt7GKsh4LMs9dKY/LgXzlvXlVr54n/VMKzqI9gRrD0/4X2vAlluSnoS6s3\n8d3S/7Hu+GQZ29fJ/CF7A4JEJTtc2Z93G8Flqu2Ypn1tDAraT0GplD/dAoGABiFX\nKoskYs7Ftbczx16KssB5yyHTctIwChqWoEgBZH7YBKKYnYD6QoRuBzO4QzIpFh/7\nQEl6Y18v7PdiHwkw+GZ5CLM7PF0xoFnPMkQZLjEQpKC3/Hh8D8X3lyI6d8jlK1ve\nFj/Vli2sutBbfz5SmpOqzWwbGaEUx9clIBnu4K0CgYEA1/g7IPZiRiWqa0KiGEee\nw3O0DiHJBsawvPX0PQAMrQByWXIB/wKsGeRRjCg72RPRtbCMAan+wNFjgN3yzwDw\nVw6O5RfU5vPHT4KfCFNerkpDhEy+SeYknPaIhU9Ouz0oiMErNeXPp8E7qyi6htAO\nX9VuqdMR8FOLC+2OUCQPqeU=\n-----END PRIVATE KEY-----\n",
+    client_email:
+        "firebase-adminsdk-wngmk@ece493-capstone.iam.gserviceaccount.com",
+    client_id: "106840766163003865864",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wngmk%40ece493-capstone.iam.gserviceaccount.com",
+};
