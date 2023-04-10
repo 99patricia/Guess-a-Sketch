929b5743e5c70ce6e1665456db18ca28c70b7b0b
Patricia Zafra <pzafra@ualberta.ca>
2023-04-09 20:34:31 -0600

    Display custom wordbanks in profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -75,27 +75,33 @@ function Profile(props) {
     };
 
     const [gameHistory, setGameHistory] = useState([]);
+    const [wordbanks, setWordbanks] = useState([]);
 
     useEffect(() => {
-
         const fetchGames = async () => {
             const games = profileData.gamehistory;
             if (games) {
                 var i;
                 setGameHistory([]);
-                for (i=0; i<games.length; i++) {
-                    await axios
-                        .get(`/games/${games[i]}`)
-                        .then((res) => {
-                            setGameHistory(oldArray => [...oldArray, res.data]);
-                        });
+                for (i = 0; i < games.length; i++) {
+                    await axios.get(`/games/${games[i]}`).then((res) => {
+                        setGameHistory((oldArray) => [...oldArray, res.data]);
+                    });
                 }
-
             }
-        }
-        
-        fetchGames()
-            .catch(console.error);
+        };
+
+        const fetchWordbanks = async () => {
+            if (userData.id) {
+                // Fetch user's wordbanks
+                await axios
+                    .get(`/wordbank/${userData.id}`)
+                    .then((res) => setWordbanks(res.data));
+            }
+        };
+
+        fetchGames().catch(console.error);
+        fetchWordbanks().catch((error) => console.error(error));
     }, [profileData]);
 
     // https://www.w3schools.com/howto/howto_js_tabs.asp
@@ -190,7 +196,7 @@ function Profile(props) {
                                 className="tabcontent"
                                 style={{ display: "none" }}
                             >
-                                <CustomWordbanks />
+                                <CustomWordbanks wordbanks={wordbanks} />
                             </div>
                         </ProfileNavContainer>
                     </>
--- client/src/components/Profile/CustomWordbanks.jsx
+++ client/src/components/Profile/CustomWordbanks.jsx
@@ -1,13 +1,48 @@
+import FormInput from "components/FormInput";
 import React from "react";
+import { Desktop } from "service/mediaQueries";
 import styled from "styled-components";
 
+const WordbankContainer = styled.div`
+    display: grid;
+    grid-gap: 1rem;
+    margin: 1rem;
+`;
+
+const WordbankSection = styled.div`
+    background-color: var(--beige);
+    padding: 1rem;
+    border-radius: 1rem 1rem 0 0;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    text-transform: uppercase;
+`;
+
+const WordbankContent = styled(FormInput)`
+    resize: none;
+    border-radius: 0 0 1rem 1rem;
+`;
+
 function CustomWordbanks(props) {
+    const isDesktop = Desktop();
+    const { wordbanks } = { ...props };
 
     return (
-        <>
-        customwordbanks
-        </>
-    )
+        <WordbankContainer>
+            {wordbanks.map((wordbank) => (
+                <>
+                    {!wordbank.isGlobal && (
+                        <div key={wordbank.name}>
+                            <WordbankSection>{wordbank.name} </WordbankSection>
+                            <WordbankContent
+                                textArea
+                                defaultValue={wordbank.words}
+                            ></WordbankContent>
+                        </div>
+                    )}
+                </>
+            ))}
+        </WordbankContainer>
+    );
 }
 
 export default CustomWordbanks;
